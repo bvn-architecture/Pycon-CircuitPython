1c8609a548235f5c59ee76b701da5d5c7fe856d5
AidenRay <AidenRay@github.com>
2018-08-23 12:33:14 +1000

    Minor additions before sleep

--- demo.py
+++ demo.py
@@ -1,16 +1,21 @@
 import sys
 import os
+import time
 
+import board
+import analogio
+
+light = analogio.AnalogIn(board.LIGHT)
 
 
 """ Check Python implementation for testing purposes. """
 if sys.implementation.name == 'circuitpython':
-    import board
     pass
 
 def main():
     """ event loop """
     while True:
-        pass
+        print(light.value)
+        time.sleep(0.1)
 if sys.implementation.name == 'circuitpython':
     main()
